a517fa463c6d097aca6f1a08bbc3d7fac9aacb13
Kevin Yan <k3vinyan@gmail.com>
2016-01-12 11:18:32 -0800

    update gemfile and create welcome controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  #Returns the full title on a per-page basis
+  def full_title(page_title = "")
+    base_title = "Movie Lovers"
+    if page_title.empty?
+      base_title
+    end
+  end
 end
